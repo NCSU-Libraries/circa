9f9bee38d935dfe5dcd5d0e04f5fe128dc2334b2
Trevor Ryan Thornton <trthorn2@ncsu.edu>
2018-12-06 16:05:38 -0500

    css update

--- lib/modules/general_utilities.rb
+++ lib/modules/general_utilities.rb
@@ -6,4 +6,18 @@ module GeneralUtilities
     end
   end
 
+  # Log message and output to stdout
+  def log_info(message)
+    Rails.logger.info message
+    # output to stdout for visibility into running background jobs
+    puts "#{DateTime.now.to_s} - #{message}"
+  end
+
+  # Log error and output to stdout
+  def log_error(message)
+    Rails.logger.error message
+    # output to stdout for visibility into running background jobs
+    puts "#{DateTime.now.to_s} - ERROR: #{message}"
+  end
+
 end
